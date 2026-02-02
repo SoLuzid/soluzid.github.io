85bec383c3c4f2337799d53c35b414a50e90c402
ElPro7777 <86174806+ElPro7777@users.noreply.github.com>
2026-02-02 20:27:52 -0300

    uh added swap cuz i forgor

--- games/games.js
+++ games/games.js
@@ -12,6 +12,18 @@ const gameList = [
     ]
   },
   {
+    page: "Swap",
+    date: "2026-02-02 20:24",
+    title: "Swap!",
+    version: "1.0.0",
+    description: "You are given scrambled words that you have to unscramble by swapping letters by the ones on their sides. you have 5 minutes to get as much score as you possibly can. im not sure if you can get more than 40 solved words tbh",
+    preview: "no thumbnail sorry i forgot to make one and im lazy :(",
+    changelog: [
+      "1.0.0: First non-beta Release. added just a new logo, that's all. that's all this damn update added. that's all. really.",
+      "0.9.0: Original Beta Release. literally added the whole game itself"
+    ]
+  },
+  {
     page: "Santa's Adventure 2 web/index.html",
     date: "2025-8-4 12:00",
     title: "Santa's Adventure 2 demo",
