358409d7b8b765c34e4d1f21980f7002db04bca4
ElPro7777 <86174806+ElPro7777@users.noreply.github.com>
2026-07-30 21:43:50 -0300

    * Well, there's not a man there.

--- header.js
+++ header.js
@@ -1,13 +1,31 @@
 const opts = document.currentScript.dataset;
+
 fetch("/header.html")
   .then(r => r.text())
   .then(h => {
     document.body.insertAdjacentHTML("afterbegin", h);
+
     const h1 = document.getElementById("dynamic-title");
-    if (h1 && document.title) h1.textContent = document.title;
+    if (h1 && document.title) {
+      h1.textContent = document.title;
+    }
+
+    const homeAnchor = document.querySelector(".home-link a");
+
     if (opts.home === "0") {
       const homeLink = document.querySelector(".home-link");
       if (homeLink) homeLink.style.pointerEvents = "none";
     }
-    
+
+    if (
+      homeAnchor &&
+      localStorage.getItem("manegg") !== "true" &&
+      Math.random() < 0.1
+    ) {
+      homeAnchor.addEventListener("click", e => {
+        e.preventDefault();
+        sessionStorage.setItem("returnUrl", location.href);
+        location.href = "/man.html";
+      });
+    }
   });
